e0d0c47c3615341c5dbf7d768b872ee66beefc38
m-menam <mohamedmenam409@gmail.com>
2025-01-13 17:01:32 +0200

    button

--- transfer-app/src/app/app.component.ts
+++ transfer-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { ImportAccountsComponent } from './import-accounts/import-accounts.component';
 import { AccountsListComponent } from './accounts-list/accounts-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
